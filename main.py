81ce16e67d565ebe6377ef3b220f8bea08669777
Ekate <simple.k4te.l@gmail.com>
2024-12-23 19:13:24 +0300

    добавила обработку команд \start и \help

--- main.py
+++ main.py
@@ -11,3 +11,24 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 dp = Dispatcher(bot)
 
+user_routes = {}
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply(
+        "Привет! Я weather bot.\n\n"
+        "Я помогу тебе узнать погоду на маршруте на заданный промежуток времени. \n"
+        "Команды: \n"
+        "/weather - Узнать прогноз погоды для маршрута\n"
+        "/help - Список команд"
+    )
+
+@dp.message_handler(commands=['help'])
+async def send_help(message: types.Message):
+    await message.reply(
+        "Команды:\n"
+        "/start - Начать работу с ботом\n"
+        "/weather - Узнать прогноз погоды для маршрута\n"
+        "/help - Список команд"
+    )
+
